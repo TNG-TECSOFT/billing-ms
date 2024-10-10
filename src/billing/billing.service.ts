a10fbbf93fe773ca81cd874fb96dd6c20d579806
Diego Ferrando <dferrando@tecnosoftware.com>
2024-10-10 16:02:58 -0300

    Added count and formatted response

--- src/billing/billing.service.ts
+++ src/billing/billing.service.ts
@@ -18,8 +18,6 @@ export class BillingService {
       await queryRunner.startTransaction();
 
       const validatedParams: Params = this.validateParamsFromPayload(params);
-      // const paramsArray = Object.values(validatedParams);
-      // paramsArray.pop();
 
       const paramsArray = [
         validatedParams.shipperId,
@@ -31,17 +29,33 @@ export class BillingService {
         validatedParams.createdAtTo
       ];
 
-      console.log(paramsArray);
-
       const query = getBillableOrdersQuery() + 
-        `ORDER BY ${validatedParams.sort} ${validatedParams.order} LIMIT ${validatedParams.limit} OFFSET ${validatedParams.offset};`;
+        `${validatedParams.sort} ${validatedParams.order} LIMIT ${validatedParams.limit} OFFSET ${validatedParams.offset};`;
+      const queryCount = `SELECT COUNT(*) 
+      FROM (` + getBillableOrdersQuery() +
+        `${validatedParams.sort} ${validatedParams.order} ) AS subquery;`;
       const result = await queryRunner.query(query, paramsArray);
+      const resultCount = await queryRunner.query(queryCount, paramsArray);
 
-      console.log(result);
+      const transformedData = this.processResult(result);
+
+      const count = resultCount[0].count;
+      const total = 0; // need to be added later
+      const page = validatedParams.page;
+      const pageCount = Math.ceil(count / validatedParams.limit);
+      const totalAmount = 0; // need to be added later
+
+      const ordersMap: Map<string, any> = new Map();
+      ordersMap.set('orders', transformedData);
+      ordersMap.set('count', count);
+      ordersMap.set('page', Number(page));
+      ordersMap.set('pageCount', pageCount);
+      ordersMap.set('total', total);
+      ordersMap.set('totalAmount', totalAmount);
       
       await queryRunner.commitTransaction();
-      
-      return result;
+
+      return ordersMap;
     } catch (error) {
       await queryRunner.rollbackTransaction();
       throw new Error(`Error executing custom query: ${error.message}`);
@@ -71,12 +85,82 @@ export class BillingService {
       momentId: parsedPayload.momentId || 0,
       createdAtFrom: parsedPayload.createdAtFrom || Date.now(),
       createdAtTo: parsedPayload.createdAtTo || Date.now(),
-      sort: parsedPayload.sort || 'id',
+      // sort: parsedPayload.sort || '"stagesHistory"."createdAt"',
+      sort: '"stagesHistory"."createdAt"',
       order: parsedPayload.order || 'DESC',
       limit: parsedPayload.limit || 10,
       offset: parsedPayload.offset || parsedPayload.limit * (parsedPayload.page - 1),
       page: parsedPayload.page || 1 // no need to use
     };
     return params
+  }
+
+  private processResult(results: Object[]): any {
+    const transformedData = results.reduce((acc: any[], order: any) => {
+        // Check if the order already exists in the transformed data
+        let existingOrder = acc.find((o: any) => o.id === order.order_id);
+
+        // If the order doesn't exist, create it
+        if (!existingOrder) {
+            existingOrder = {
+                id: order.order_id,
+                trackingId: order.order_trackingId,
+                address: order.order_address.trim(),
+                zipCode: order.order_zipCode,
+                province: order.order_province,
+                state: order.order_state,
+                shipper: {
+                    id: order.shipper_id,
+                    name: order.shipper_name,
+                },
+                service: {
+                    id: order.service_id,
+                },
+                chanelledNode: {
+                    id: order.chanelledNode_id,
+                    name: order.chanelledNode_name,
+                },
+                pieces: [],
+                product: {
+                    id: order.product_id,
+                    productShippers: [
+                        {
+                            dimensionalFactor: order.productShipper_dimensionalFactor,
+                        },
+                    ],
+                },
+                billingAmount: 500, // Placeholder, can be dynamically set if needed
+            };
+            acc.push(existingOrder);
+        }
+
+        // Create the piece object and add it to the order's pieces array
+        const piece = {
+            id: order.piece_id,
+            SKU: order.piece_SKU,
+            height: order.piece_height,
+            width: order.piece_width,
+            length: order.piece_length,
+            weight: order.piece_weight,
+            stagesHistory: [
+                {
+                    id: order.stagesHistory_id,
+                    createdAt: new Date(order.stagesHistory_createdAt),
+                    moment: order.moment_id ? {
+                        id: order.moment_id,
+                        displayName: order.moment_display_name,
+                    } : null,
+                },
+            ],
+            price: 500, // Placeholder, can be dynamically set if needed
+        };
+
+        // Add the piece to the existing order's pieces array
+        existingOrder.pieces.push(piece);
+
+        return acc;
+    }, []);
+
+    return transformedData;
   }
 }
