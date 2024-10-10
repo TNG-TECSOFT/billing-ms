c2b050ae17cf6577f2c5eadf454a4be933cfa05e
Diego Ferrando <dferrando@tecnosoftware.com>
2024-10-10 16:44:44 -0300

    Resolved conflict

--- src/common/constants/queries.ts
+++ src/common/constants/queries.ts
@@ -1,4 +1,4 @@
-export function getBillableOrdersQuery() {
+function getBillableOrdersQuery() {
   return `
   SELECT  DISTINCT ON ("order"."id", "piece"."id")
     "order"."id" AS "order_id", 
@@ -50,120 +50,9 @@ export function getBillableOrdersQuery() {
   ORDER BY 
     "order"."id", "piece"."id", 
   `;
-  /* SELECT DISTINCT ON ("piece"."id")
-      "order"."id" AS "orderId",
-      "order"."trackingId",
-      "order"."address",
-      "order"."zipCode",
-      "order"."province",
-      "order"."state",
-      "order"."product",
-      "shipper"."id" AS "shipperId",
-      "shipper"."name" AS "shipperName",
-      "product"."id" AS "productId",
-      "productShipper"."dimensionalFactor",
-      "services"."id" AS "serviceId",
-      "chanelledNode"."id" AS "chanelledNodeId",
-      "chanelledNode"."name" AS "chanelledNodeName",
-      "piece"."id" AS "pieceId",
-      "piece"."height",
-      "piece"."width",
-      "piece"."SKU",
-      "piece"."length",
-      "piece"."weight",
-      "stagesHistory"."id" AS "stagesHistoryId",
-      "stagesHistory"."createdAt" AS "stagesHistoryCreatedAt",
-      "moment"."id" AS "momentId",
-      "moment"."display_name" AS "momentDisplayName"
-    FROM "order"
-      LEFT JOIN "piece" ON "order"."id" = "piece"."orderId"
-      LEFT JOIN "shipper" ON "order"."shipper" = "shipper"."id"
-      LEFT JOIN "stage" ON "order"."stageId" = "stage"."id"
-      LEFT JOIN "services" ON "order"."service" = "services"."id"
-      LEFT JOIN "node" AS "chanelledNode" ON "order"."chanelledNodeId" = "chanelledNode"."id"
-      LEFT JOIN "stages_history" AS "stagesHistory" ON "piece"."id" = "stagesHistory"."pieceId"
-      LEFT JOIN "stage" as "recordedStage" ON "stagesHistory"."stageId" = "recordedStage"."id"
-      LEFT JOIN "moments" AS "moment" ON "stagesHistory"."momentId" = "moment"."id"
-      LEFT JOIN "product" ON "order"."product" = "product"."id"
-      LEFT JOIN "product_shipper" AS "productShipper" ON "shipper"."id" = "productShipper"."shipperId"
-      LEFT JOIN "product_shipper_products_product" ON "product"."id" = "product_shipper_products_product"."productId"
-      AND "product_shipper_products_product"."productShipperId" = "productShipper"."id"
-    WHERE 
-      "order"."shipper" = $1
-      AND ("order"."service" = $2 OR $2 = '0')
-      AND ("order"."product" = $3 OR $3 = '0')
-      AND ("order"."originalTrackingId" = $4 OR $4 = '0')
-<<<<<<< HEAD
-      AND ("moment"."id" = $5 OR $5 = 0)
-      AND "stagesHistory"."createdAt" BETWEEN $6 AND $7
-    ORDER BY 
-      "order"."id", "piece"."id", "stagesHistory"."createdAt" DESC
-    LIMIT 10 OFFSET 0;
-  `; */
-=======
-      AND ("moments"."id" = $5 OR $5 = 0)
-      AND "stages_history"."createdAt" BETWEEN $6 AND $7
-     `;
-    
-    //  ORDER BY 
-    //   "stagesHistoryCreatedAt" DESC
-    // LIMIT $8 OFFSET $9;
- 
-
-  // return `
-  //   SELECT 
-  //     "order"."id",
-  //     "order"."originalTrackingId",
-  //     "order"."address",
-  //     "order"."zipCode",
-  //     "order"."province",
-  //     "order"."state",
-  //     "order"."product",
-  //     "product_shipper"."shipperId",
-  //     "shipper"."name" AS "shipperName",
-  //     "product_shipper_products"."product_id",
-  //     "product_shipper"."dimensionalFactor",
-  //     "services"."id" AS "serviceId",
-  //     -- "chanelledNode"."id" AS "chanelledNodeId",
-  //     -- "chanelledNode"."name" AS "chanelledNodeName",
-  //     "piece"."id" AS "pieceId",
-  //     "piece"."height",
-  //     "piece"."width",
-  //     "piece"."SKU",
-  //     "piece"."length",
-  //     "piece"."weight",
-  //     "stages_history"."id" AS "stagesHistoryId",
-  //     "stages_history"."createdAt" AS "stagesHistoryCreatedAt",
-  //     "moments"."id" AS "momentId",
-  //     "moments"."display_name" AS "momentDisplayName"
-  //   FROM "order"
-  //   LEFT JOIN "shipper" ON "order"."shipper" = "shipper"."id"
-  //   LEFT JOIN "stage" ON "order"."stageId" = "stage"."id"
-  //   LEFT JOIN "services" ON "order"."service" = "services"."id"
-  //   -- LEFT JOIN "chanelledNode" ON "order"."chanelledNodeId" = "chanelledNode"."id"
-  //   LEFT JOIN "piece" ON "order"."id" = "piece"."orderId"
-  //   LEFT JOIN "stages_history" ON "piece"."id" = "stages_history"."pieceId"
-  //   -- LEFT JOIN "recordedStage" ON "stages_history"."recordedStageId" = "recordedStage"."id"
-  //   LEFT JOIN "moments" ON "stages_history"."momentId" = "moments"."id"
-  //   LEFT JOIN "product" ON "order"."product" = "product"."id"
-  //   LEFT JOIN "product_shipper" ON "shipper"."id" = "product_shipper"."shipperId"
-  //   LEFT JOIN "product_shipper_products" ON "product_shipper"."id" = "product_shipper_products"."product_shipper_id"
-  //   AND "product_shipper_products"."product_id" = "product"."id"
-  //   WHERE 
-  //     "shipper"."id" = $1
-  //     AND "services"."id" = $2
-  //     AND "product_shipper_products"."product_id" = $3
-  //     -- AND "order"."impositionPlaceId" = $4
-  //     AND "order"."originalTrackingId" = $5
-  //     -- AND "chanelledNode"."name" = $6
-  //     AND "moments"."display_name" = $7
-  //     AND "stages_history"."createdAt" BETWEEN $8 AND $9
-  //   ORDER BY 
-  //     "stagesHistoryCreatedAt" DESC
-  //   LIMIT $12 OFFSET $13;
-  // `;
 }
-export function getAddOrdersToSendQuery(){
+
+function getAddOrdersToSendQuery(){
   return `
   INSERT INTO "order_to_billing" (
     "toMonth", 
@@ -223,7 +112,7 @@ export function getAddOrdersToSendQuery(){
   `
 }
 
-export function getOrdersToSendQuery(){
+function getOrdersToSendQuery(){
   return `
     SELECT 
     s.name AS shipper,
@@ -251,5 +140,6 @@ export function getOrdersToSendQuery(){
   LEFT JOIN 
   node as n on o.id = n.id
   `
->>>>>>> f5822e648a7d9417ae382f7f34927fee8608cd08
 }
+
+export { getBillableOrdersQuery, getAddOrdersToSendQuery, getOrdersToSendQuery };
