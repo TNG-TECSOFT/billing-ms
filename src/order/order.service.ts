3030bfac0e0b7ed617a58074a6f217338fe5ed04
Diego Ferrando <dferrando@tecnosoftware.com>
2024-10-21 14:46:30 -0300

    Removed unused dependencies and fixed data types

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/typeorm';
-import { Connection, QueryRunner } from 'typeorm';
-import { getAddOrdersToSendQuery } from '../common/constants/queries';
+import { Connection } from 'typeorm';
 import { AddOrderToBillingRequestDto } from './dto/add-order-to-billing.dto';
 import { OrderToBillingRepository } from './repositories/order-to-billing.repository';
 import { GetOrderToBillingDto } from './dto/get-order-to-billing.dto';
-import { Params } from '../billing/dto/billable-orders-request.dto';
 import { BillingRepository } from '../billing/billing.repository';
 import { BillingService } from '../billing/billing.service';
 import { BillableOrdersDto } from '../billing/dto/billable-orders.dto';
@@ -15,16 +13,15 @@ export class OrderService {
   constructor(
     @InjectConnection() private readonly connection: Connection,
     private readonly orderToBillingRepository: OrderToBillingRepository,
-    private readonly billingRepository: BillingRepository,
     private readonly billingService: BillingService
   ) {}
 
   async addOrderToBilling(request: AddOrderToBillingRequestDto): Promise<any> {
     // Get billable orders
-    const orders: BillableOrdersDto[] = await this.billingService.getBillableOrders(request.params, request.authorization_core);
+    const orders = await this.billingService.getBillableOrders(request.params, request.authorization_core);
 
     // Add orders to billing
-    const result = await this.orderToBillingRepository.addOrdersToBilling(orders, request.token);
+    const result = await this.orderToBillingRepository.addOrdersToBilling(orders.data, request.token);
 
     return result;
   }
